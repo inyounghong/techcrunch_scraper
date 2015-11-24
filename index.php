c56a9ed728af3331a7f4445d5e2364c4eb33afd7
Inyoung Hong <ih235@cornell.edu>
2015-11-24 16:00:11 -0500

    scraper

--- index.php
+++ index.php
@@ -1,35 +1,35 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<style>
-		body{
-			padding: 50px;
-		}
+    <style>
+        body{
+            padding: 50px;
+        }
 
-		img{
-			max-height: 200px;
-		}
+        img{
+            max-height: 200px;
+        }
 
-	</style>
+    </style>
 </head>
-	<body>
-		<?php ini_set('display_errors', 'On'); ?>
+    <body>
+        <?php ini_set('display_errors', 'On'); ?>
 
-		<form action="" method="post">
-			<input type="text" name="search_query" placeholder="Search Query">
-			<input type="submit" value="Search" name="search">
-		</form>
+        <form action="" method="post">
+            <input type="text" name="search_query" placeholder="Search Query">
+            <input type="submit" value="Search" name="search">
+        </form>
 
-		<form action="" method="post">
-			<input type="submit" value="Scrape Techcrunch" name="scrape">
-		</form>
+        <form action="" method="post">
+            <input type="submit" value="Scrape Techcrunch" name="scrape">
+        </form>
 
-		<form action="" method="post">
-			<input type="submit" value="Reset Database" name="reset">
-		</form>
+        <form action="" method="post">
+            <input type="submit" value="Reset Database" name="reset">
+        </form>
 
-		<?php include "scraper.php" ?>
-		<?php include "search.php" ?>
+        <?php include "scraper.php" ?>
+        <?php include "search.php" ?>
 
-	</body>
+    </body>
 </html>
--- search.php
+++ search.php
@@ -3,44 +3,44 @@
 // On search
 if (isset($_POST['search'])){
 
-	// Connect to mysql
-	$link = mysqli_connect("127.0.0.1", "testdb_admin", "majesticeagle", "testdb");
+    // Connect to mysql
+    $link = mysqli_connect("127.0.0.1", "testdb_admin", "majesticeagle", "testdb");
 
-	if (!$link) {
-	    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-	    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-	    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-	    exit;
-	}
+    if (!$link) {
+        echo "Error: Unable to connect to MySQL." . PHP_EOL;
+        echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+        exit;
+    }
 
-	// Select query
-	$q = mysqli_real_escape_string($link, $_POST['search_query']);
-	$query = "SELECT * FROM Articles WHERE MATCH(title, body, description) AGAINST ('$q' IN BOOLEAN MODE)";
-	$r = mysqli_query($link, $query);
+    // Select query
+    $q = mysqli_real_escape_string($link, $_POST['search_query']);
+    $query = "SELECT * FROM Articles WHERE MATCH(title, body, description) AGAINST ('$q' IN BOOLEAN MODE)";
+    $r = mysqli_query($link, $query);
 
-	// Display number of results
-	echo mysqli_num_rows($r) . " result(s).";
-	if (mysqli_num_rows($r) == 0){
-		echo " Query is too general or too specific.";
-	}
+    // Display number of results
+    echo mysqli_num_rows($r) . " result(s).";
+    if (mysqli_num_rows($r) == 0){
+        echo " Query is too general or too specific.";
+    }
 
-	if ($r) {
-		// Display each result
-		while ($row = mysqli_fetch_array($r)) {
-			print  "<div class='article'>
-						<h1>{$row['title']}</h1>
-						<p><b>Description</b>: {$row['description']}</p>
-						<p><b>Body</b>: {$row['body']}</p>
-						<img src='{$row['image']}'>
-					</div>";
-		}
-	}
-	else{
-		// If error with select
-		print 'Could not retrieve data because ' . mysqli_error($link) . '<br>the query was ' . $query;
-	}
+    if ($r) {
+        // Display each result
+        while ($row = mysqli_fetch_array($r)) {
+            print  "<div class='article'>
+                        <h1>{$row['title']}</h1>
+                        <p><b>Description</b>: {$row['description']}</p>
+                        <p><b>Body</b>: {$row['body']}</p>
+                        <img src='{$row['image']}'>
+                    </div>";
+        }
+    }
+    else{
+        // If error with select
+        print 'Could not retrieve data because ' . mysqli_error($link) . '<br>the query was ' . $query;
+    }
 
-	mysqli_close($link);
+    mysqli_close($link);
 }
 
 ?>
